f5a2f1407902d1772527ef680eb06723c0238e45
Tom Chen <developer@tomchentw.com>
2021-05-18 03:15:10 +0800

    fix(next.config): add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 export default {
   basePath: "/taipower", // GitHub pages
+  assetPrefix: "/taipower/",
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
