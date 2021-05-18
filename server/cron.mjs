7fe88ff41422478ad7aa42e4df3bff89b3481650
Tom Chen <developer@tomchentw.com>
2021-05-18 16:53:02 +0800

    feat(cron): write current time

--- server/cron.mjs
+++ server/cron.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import fetch from "node-fetch";
 
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+const TIMEPATH = path.join(DIRNAME, `../public/data/time`);
 const RAW_DIR = path.join(DIRNAME, `../public/data/raw`);
 
 const URL_LIST = [
@@ -20,7 +21,8 @@ async function run() {
       const text = await fetch(url).then((r) => r.text());
       const dataPath = path.join(RAW_DIR, pathname);
       await fs.promises.writeFile(dataPath, text);
-    })
+    }),
+    fs.promises.writeFile(TIMEPATH, `${Date.now()}`)
   );
 }
 
