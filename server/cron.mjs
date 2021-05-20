979acb892244f3ecaf1676fbeee25fdecb6e81e8
Tom Chen <developer@tomchentw.com>
2021-05-20 17:17:03 +0800

    feat(server/cron): copy data folder from gh-pages

--- server/cron.mjs
+++ server/cron.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import spawn from "cross-spawn";
 import crawl from "./crawl.mjs";
 import accumulate from "./accumulate.mjs";
 import logger from "./logger.mjs";
@@ -8,6 +9,15 @@ import logger from "./logger.mjs";
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
 
 async function run() {
+  // Prepare data folder to work with
+  if (!process.env.CI) {
+    spawn.sync("rm", ["-rf", "public/data"], { stdio: "inherit" });
+  }
+  spawn.sync("git", ["restore", "--source=origin/gh-pages", "data"], {
+    stdio: "inherit",
+  });
+  spawn.sync("mv", ["data", "public/data"], { stdio: "inherit" });
+
   await crawl();
   await accumulate();
 }
