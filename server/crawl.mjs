4fdb47a5fbd7533280e9e416ff48c4cab1debc02
Tom Chen <developer@tomchentw.com>
2021-05-20 16:01:18 +0800

    feat(server/crawl): create `fetch` with `@vercel/fetch`

--- server/crawl.mjs
+++ server/crawl.mjs
@@ -1,8 +1,9 @@
 import fs from "fs";
 import path from "path";
-import https from "https";
 import { fileURLToPath } from "url";
-import fetch from "node-fetch";
+import createFetch from "@vercel/fetch";
+
+const fetch = createFetch();
 
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
 const RAW_DIR = path.join(DIRNAME, `../public/data/raw`);
@@ -19,10 +20,6 @@ const URL_LIST = [
   },
 ];
 
-const httpsAgent = new https.Agent({
-  keepAlive: true,
-});
-
 export default async function crawl() {
   await fs.promises.mkdir(RAW_DIR, { recursive: true });
   await Promise.all(
@@ -30,7 +27,7 @@ export default async function crawl() {
       while (true) {
         const dataPath = path.join(RAW_DIR, pathname);
         const [text, prevText] = await Promise.all([
-          fetch(url, { agent: httpsAgent }).then((r) => r.text()),
+          fetch(url).then((r) => r.text()),
           fs.promises.readFile(dataPath, "utf-8").catch((error) => {
             if ("ENOENT" !== error.code) {
               throw error;
