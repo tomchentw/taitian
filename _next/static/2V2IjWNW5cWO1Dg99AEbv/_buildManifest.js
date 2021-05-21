cd6ade4804def0ccb64e8a197bfc4e22103cfd6c
tomchentw <tomchentw@users.noreply.github.com>
2021-05-21 09:54:13 +0000

    deploy: 9d7124634727f89c5cb193b82fe95a59e37788ce

---
+++ _next/static/2V2IjWNW5cWO1Dg99AEbv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/696-9f645324ee065d874688.js","static/chunks/pages/index-2372820b74a07767138b.js"],"/_error":["static/chunks/pages/_error-6e2d0661cbf2109e89ce.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
