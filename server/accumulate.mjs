643381142a718d29b4a6a577db54501744924610
Tom Chen <developer@tomchentw.com>
2021-05-23 00:50:53 +0800

    fix(server/accumulate): switch to genaryToCSV

--- server/accumulate.mjs
+++ server/accumulate.mjs
@@ -65,6 +65,41 @@ async function genary(accumulatedDirPath) {
   );
 }
 
+async function genaryToCSV(accumulatedDirPath) {
+  const sourceFilePath = path.join(RAW_DIR, "genary.json");
+  const accumulatedFilePath = path.join(accumulatedDirPath, "genary.csv");
+  const [{ "": datetime, aaData }, accumulatedCSV] = await Promise.all([
+    fs.promises.readFile(sourceFilePath, "utf-8").then(parseJson),
+    fs.promises.readFile(accumulatedFilePath, "utf-8").catch((error) => {
+      if ("ENOENT" !== error.code) {
+        throw error;
+      }
+      return "time,type,name,capacity,generation,percentage,note\n";
+    }),
+  ]);
+  const [date, time] = datetime.split(" ");
+  function transformToEntry([
+    htmlType,
+    name,
+    capacity,
+    generation,
+    percentage,
+    rawNote,
+  ]) {
+    const [_, type] = htmlType.match(/^<A NAME='(\S+)'>.+$/);
+    const note = rawNote.trim();
+    return [time, type, name, capacity, generation, percentage, note];
+  }
+  const lastEntry = transformToEntry(_.last(aaData));
+  if (_.isEqual(_.last(accumulatedCSV.split("\n")), lastEntry)) {
+    return;
+  }
+  const appendedCSV = aaData
+    .map(transformToEntry)
+    .reduce((acc, it) => `${acc}${it.join(",")}\n`, "");
+  await fs.promises.appendFile(accumulatedFilePath, appendedCSV);
+}
+
 async function loadfueltype(accumulatedDirPath) {
   const sourceFilePath = path.join(RAW_DIR, "loadfueltype.csv");
   const accumulatedFilePath = path.join(accumulatedDirPath, "loadfueltype.csv");
@@ -91,7 +126,7 @@ export default async function accumulate() {
   const accumulatedDirPath = path.join(ACCUMULATED_DIR, year, month, day);
   await fs.promises.mkdir(accumulatedDirPath, { recursive: true });
   // Every interval
-  await Promise.all([genary(accumulatedDirPath)]);
+  await Promise.all([genaryToCSV(accumulatedDirPath)]);
   // Only before Midnight
   if (new Date() < closeToMindnight) {
     return;
