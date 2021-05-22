06bf061f38b6e2f252009a361eb38f62f51ade28
Tom Chen <developer@tomchentw.com>
2021-05-22 21:03:43 +0800

    feat(server/accumulate): collect genary

--- server/accumulate.mjs
+++ server/accumulate.mjs
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import * as _ from "lodash-es";
+import logger from "./logger.mjs";
 
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
 const RAW_DIR = path.join(DIRNAME, `../public/data/raw`);
@@ -13,6 +14,56 @@ const taipeiFormat = new Intl.DateTimeFormat("en", {
   day: "2-digit",
   timeZone: "Asia/Taipei",
 });
+
+function parseJson(string) {
+  return JSON.parse(string);
+}
+
+async function genary(accumulatedDirPath) {
+  const sourceFilePath = path.join(RAW_DIR, "genary.json");
+  const accumulatedFilePath = path.join(accumulatedDirPath, "genary.json");
+  const [{ "": datetime, aaData }, accumulated] = await Promise.all([
+    fs.promises.readFile(sourceFilePath, "utf-8").then(parseJson),
+    fs.promises
+      .readFile(accumulatedFilePath, "utf-8")
+      .then(parseJson, (error) => {
+        if ("ENOENT" !== error.code) {
+          throw error;
+        }
+        return {
+          date: "",
+          data: [],
+        };
+      }),
+  ]);
+  const [date, time] = datetime.split(" ");
+  if (!accumulated.date) {
+    accumulated.date = date;
+  }
+  if (accumulated.date !== date) {
+    // Don't do anything
+    logger.error("[accumulate#genary] date mismatch!", accumulated.date, date);
+    return;
+  }
+  const entry = {
+    time,
+    generators: aaData.map(
+      ([htmlType, name, capacity, generation, percentage, rawNote]) => {
+        const [_, type] = htmlType.match(/^<A NAME='(\S+)'>.+$/);
+        const note = rawNote.trim();
+        return { type, name, capacity, generation, percentage, note };
+      }
+    ),
+  };
+  if (_.isEqual(_.last(accumulated.data), entry)) {
+    return;
+  }
+  accumulated.data.push(entry);
+  await fs.promises.writeFile(
+    accumulatedFilePath,
+    JSON.stringify(accumulated, null, 2)
+  );
+}
 
 async function loadfueltype(accumulatedDirPath) {
   const sourceFilePath = path.join(RAW_DIR, "loadfueltype.csv");
@@ -35,13 +86,16 @@ export default async function accumulate() {
     23,
     50
   );
-  if (now < closeToMindnight) {
-    return;
-  }
   const [{ value: month }, , { value: day }, , { value: year }] =
     taipeiFormat.formatToParts(closeToMindnight);
   const accumulatedDirPath = path.join(ACCUMULATED_DIR, year, month, day);
   await fs.promises.mkdir(accumulatedDirPath, { recursive: true });
+  // Every interval
+  await Promise.all([genary(accumulatedDirPath)]);
+  // Only before Midnight
+  if (new Date() < closeToMindnight) {
+    return;
+  }
   await Promise.all([
     loadfueltype(accumulatedDirPath),
     loadareas(accumulatedDirPath),
