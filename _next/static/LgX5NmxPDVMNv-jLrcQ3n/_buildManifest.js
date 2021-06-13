379a4de12423cb00d06b9cb83c69509030c22841
tomchentw <tomchentw@users.noreply.github.com>
2021-06-13 17:36:24 +0000

    deploy: 6c4ed13dc96d2143dc9df21613959093d97dffe5

--- _next/static/LgX5NmxPDVMNv-jLrcQ3n/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/671-42e4da0e5a863a633baf.js","static/chunks/pages/index-e6bf5af115ab0e65e9e9.js"],"/_error":["static/chunks/pages/_error-7ecf4bed1930c50ac9c8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tUhcRykkYr-kaNntA4Xga/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/671-42e4da0e5a863a633baf.js","static/chunks/pages/index-6799e273f6307428b268.js"],"/_error":["static/chunks/pages/_error-7ecf4bed1930c50ac9c8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
